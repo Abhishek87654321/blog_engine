9c2a2964bd7e14730bbb5f7ae0bf4f8cd662f4f0
Abhishek87654321 <abhishekgangdhari@gmail.com>
2024-02-06 09:26:43 +0530

    fixed asset precompileation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 BlogEngine::Engine.routes.draw do
+  # localhost:3000/posts
+  # because this is a engine, and it is named blog_engine
+  # This becomes :after => "localhost:3000/blog_engine/posts"
   resources :posts
 end
